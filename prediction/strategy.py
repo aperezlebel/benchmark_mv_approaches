c1fa67ada81593f975a7f695abc455d8a34bbef7
Alexandre Perez <alexperez@wanadoo.fr>
2020-02-18 17:17:37 -0500

    Turned startegy into a dataclass

--- prediction/strategy.py
+++ prediction/strategy.py
@@ -1,21 +1,26 @@
 """Implement the Strategy class."""
+from dataclasses import dataclass
+from typing import Any, Callable
+from sklearn.base import BaseEstimator
 
 
+@dataclass()
 class Strategy():
+    """Elements to run cross-validated ML estimator with hyperparms tuning."""
 
-    def __init__(self, estimator, split, cv, param_space,
-                 search):
+    estimator: BaseEstimator
+    split: Any
+    cv: Any
+    param_space: dict
+    search: Callable[[BaseEstimator, dict, Any], Any]
 
-        self.estimator = estimator
-        self.split = split
-        self.cv = cv
+    def __post_init__(self):
+        """Check params and intialize search."""
+        # Check
+        e = self.estimator
+        p = self.param_space
+        if not all(p in e.get_params().keys() for p in p.keys()):
+            raise ValueError('Given parmameters must be params of estimator.')
 
-        if not all(p in estimator.get_params().keys() for p in param_space.keys()):
-            raise ValueError('Given parmameters must all be params of estimator.')
-        self.param_space = param_space
-        self._search = search
-
-
-    @property
-    def search(self):
-        return self._search(self.estimator, self.param_space, self.cv)
+        # Intitialize search function with given parameters
+        self.search = self.search(self.estimator, self.param_space, self.cv)
