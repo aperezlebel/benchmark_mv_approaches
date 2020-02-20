fe01c7a73d2552df0a5fcfa0ca3ca075942b9da2
Alexandre Perez <alexperez@wanadoo.fr>
2020-02-20 17:24:03 -0500

    Turned into a class: DumpHelper

--- prediction/dump.py
+++ prediction/dump.py
@@ -1,12 +1,17 @@
-"""Function to dump task info and fit results into csv/yaml files."""
+"""Class to dump task info and fit results into csv/yaml files."""
 import pandas as pd
+import numpy as np
 import os
 import yaml
-# from datetime import datetime
 
 results_folder = 'results/'
 
-def dump_infos(item, filepath):
+def _dump_yaml(data, filepath):
+    data = listify(data)
+    with open(filepath, 'w') as file:
+        file.write(yaml.dump(data, allow_unicode=True))
+
+def _dump_infos(item, filepath):
     """Dump the infos at the given place.
 
     Parameters
@@ -17,17 +22,60 @@ def dump_infos(item, filepath):
         Path to the place to store the infos.
     """
     data = item.get_infos()
-    with open(filepath, 'w') as file:
-        file.write(yaml.dump(data, allow_unicode=True))
+    _dump_yaml(data, filepath)
+
+def listify(d):
+    """Convert all numpy arrays contained in the dict to lists.
+
+    Parameters
+    ----------
+    d : dict
+
+    Returns
+    -------
+    dict
+        Same dict with numpy arrays converted to lists
+
+    """
+    for k, v in d.items():
+        if isinstance(v, np.ndarray):
+            d[k] = v.tolist()
+        elif isinstance(v, dict):
+            d[k] = listify(v)
+    return d
 
 
-def dump_task_strategy(task, strategy):
-    """Dump the infos of the task and strategy used."""
-    meta = task.meta
-    task_folder = f'{results_folder}{meta.db}/{task.count}_{meta.name}/'
-    strategy_folder = f'{task_folder}{strategy.count}_{strategy.estimator_class()}/'
+class DumpHelper:
 
-    os.makedirs(strategy_folder, exist_ok=True)
 
-    dump_infos(task, f'{task_folder}task_infos.yml')
-    dump_infos(strategy, f'{strategy_folder}strat_infos.yml')
+    def __init__(self, task, strat):
+        self.task = task
+        self.strat = strat
+
+        self.task_folder = (f'{results_folder}{self.task.meta.db}/'
+                            f'{self.task.meta.name}/')
+        self.strat_folder = f'{self.task_folder}{strat.name}/'
+
+        self.dump_infos()
+
+    def dump_infos(self):
+        """Dump the infos of the task and strategy used."""
+
+        os.makedirs(self.strat_folder, exist_ok=True)
+
+        _dump_infos(self.task, f'{self.task_folder}task_infos.yml')
+        _dump_infos(self.strat, f'{self.strat_folder}strat_infos.yml')
+
+    def dump_regression(self, y_pred, y_true):
+        df = pd.DataFrame({
+            'y_pred': y_pred,
+            'y_true': y_true,
+        })
+
+        df.to_csv(self.strat_folder+'regression_results.csv')
+
+    def dump_best_params(self, best_params):
+        _dump_yaml(best_params, self.strat_folder+'best_params.yml')
+
+    def dump_cv_results(self, cv_results):
+        _dump_yaml(cv_results, self.strat_folder+'cv_results.yml')
