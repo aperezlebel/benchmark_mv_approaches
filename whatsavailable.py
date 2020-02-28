e37f83947e9eee45df7b515ae052083ab93bcc1f
Alexandre Perez <alexperez@wanadoo.fr>
2020-02-28 15:01:16 -0500

    Added a helper to know what strategy/task is available

---
+++ whatsavailable.py
@@ -0,0 +1,30 @@
+"""Helper to knwo what strategies and tasks are availables."""
+import sys
+
+from prediction.tasks import tasks
+from prediction.strategies import strategies
+
+
+def strategies_available():
+    print('\nStrategies available:')
+    for name in strategies.keys():
+        print('\t'+name)
+    print()
+
+
+def tasks_available():
+    print('\nTasks available:')
+    for name in tasks.keys():
+        print('\t'+name)
+    print()
+
+
+if 'strategies' in sys.argv:
+    strategies_available()
+elif 'tasks' in sys.argv:
+    tasks_available()
+elif len(sys.argv) == 1:
+    strategies_available()
+    tasks_available()
+else:
+    raise ValueError('Unrecognized argument(s).')
