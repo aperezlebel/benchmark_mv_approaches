18092481537c88b8b8f5c3e1db273f05f141b456
Alexandre Perez <alexperez@wanadoo.fr>
2020-03-05 13:49:33 -0500

    Added light version of fluid_intelligence

--- prediction/tasks/UKBB.py
+++ prediction/tasks/UKBB.py
@@ -1,4 +1,5 @@
 """Build prediction tasks for the UKBB database."""
+import copy
 
 from .taskMeta import TaskMeta
 
@@ -61,3 +62,53 @@ tasks_meta.append(TaskMeta(
     ],
     transform=transform_df_fluid_intelligence
 ))
+
+tasks_meta.append(TaskMeta(
+    name='fluid_intelligence_light',
+    db='UKBB',
+    df_name='24440',
+    predict='20016-0.0',
+    drop=[
+        '20016-1.0',
+        '20016-2.0'
+    ],
+    drop_contains=[
+        '10136-',
+        '10137-',
+        '10138-',
+        '10141-',
+        '10144-',
+        '10609-',
+        '10610-',
+        '10612-',
+        '10721-',
+        '10722-',
+        '10740-',
+        '10827-',
+        '10860-',
+        '10895-',  # pilots
+        '20128-',
+        '4935-',
+        '4946-',
+        '4957-',
+        '4968-',
+        '4979-',
+        '4990-',
+        '5001-',
+        '5012-',
+        '5556-',
+        '5699-',
+        '5779-',
+        '5790-',
+        '5866-',  # response
+        '40001-',
+        '40002-',
+        '41202-',
+        '41204',
+        '20002-',  # large code
+        '40006',
+    ],
+    transform=transform_df_fluid_intelligence
+))
+
+
