30f04553b3a575ad4568796eff9ade11cfa26457
Alexandre Perez <alexperez@wanadoo.fr>
2020-03-06 17:05:47 -0500

    Splited dataset to simulate the CV of randomsearch

--- test_fit_time.py
+++ test_fit_time.py
@@ -2,6 +2,7 @@
 import logging
 from sklearn.experimental import enable_hist_gradient_boosting
 from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.model_selection import train_test_split
 
 from prediction.tasks import tasks
 
@@ -17,7 +18,14 @@ def run():
     logger.info('Getting y.')
     y = task.y
 
-    logger.info(f'X shape: {X.shape}')
+    # Simulate the outer CV (the one of KFold)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    # Simulate the inner CV (the one of RandomSearchCV)
+    X_train2, X_test2, y_train2, y_test2 = train_test_split(X_train, y_train, test_size=0.2)
+
+    # Now X has the same shape as in real experiment
+    logger.info(f'X shape: {X_train2.shape}')
 
     estimator = HistGradientBoostingRegressor(
         loss='least_absolute_deviation',
@@ -26,7 +34,7 @@ def run():
     )
 
     logger.info('Fitting estimator.')
-    estimator.fit(X, y)
+    estimator.fit(X_train2, y_train2)
     logger.info('Estimator fitted.')
 
 
