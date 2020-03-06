4b740f86a0097b5420268f0802290a8b3c557e79
Alexandre Perez <alexperez@wanadoo.fr>
2020-03-06 11:24:33 -0500

    Redirect print to file for computation on the cluster

--- main_prediction.py
+++ main_prediction.py
@@ -1,8 +1,11 @@
 import logging
+import sys
 from prediction.main import main
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())  # Print also in console.
+
+sys.stdout = open('prediction_print.log', 'w')
 
 logging.basicConfig(filename='prediction.log', filemode='w', level=logging.INFO)#, format='[%(asctime)s] %(message)s')
 logger.info('Started prediction')
