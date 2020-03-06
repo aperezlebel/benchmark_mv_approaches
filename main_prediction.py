772a75623a6838eadeb252f6cd1f83918eeeb670
Alexandre Perez <alexperez@wanadoo.fr>
2020-03-06 11:38:38 -0500

    Added more infos to logs (datetime...)

--- main_prediction.py
+++ main_prediction.py
@@ -8,7 +8,13 @@ logger.addHandler(logging.StreamHandler())  # Print also in console.
 print_file = open('prediction_print.log', 'w')
 sys.stdout = print_file
 
-logging.basicConfig(filename='prediction.log', filemode='w', level=logging.INFO)#, format='[%(asctime)s] %(message)s')
+logging.basicConfig(
+    filename='prediction.log',
+    filemode='w',
+    level=logging.INFO,
+    format='%(asctime)s.%(msecs)03d:%(levelname)s:%(module)s.%(funcName)s: %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+)
 logger.info('Started prediction')
 main()
 logger.info('Ended prediction')
