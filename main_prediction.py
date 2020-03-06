a963d4656f6b54d8b7d8d8311a6bd4cd6ee3cc71
Alexandre Perez <alexperez@wanadoo.fr>
2020-03-06 11:53:29 -0500

    Separate log file at each run of the program (to run on the cluster)

--- main_prediction.py
+++ main_prediction.py
@@ -1,15 +1,46 @@
 import logging
 import sys
+import os
 from prediction.main import main
+
+logs_folder = 'logs/'
 
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())  # Print also in console.
 
-print_file = open('prediction_print.log', 'w')
+def get_and_increase_count():
+    count_filepath = logs_folder+'count.txt'
+
+    if not os.path.exists(count_filepath):
+        count = 0
+    else:
+        with open(count_filepath, 'r') as file:
+            c = file.read()
+            if c == '':
+                count = 0
+            else:
+                count = int(c) + 1
+
+    # Dump new count
+    os.makedirs(logs_folder, exist_ok=True)
+    with open(count_filepath, 'w') as file:
+        file.write(str(count))
+
+    return count
+
+
+count = get_and_increase_count()
+
+
+def get_log_filepath(filename):
+    return f'{logs_folder}{count}_{filename}'
+
+
+print_file = open(get_log_filepath('prediction_print.log'), 'w')
 sys.stdout = print_file
 
 logging.basicConfig(
-    filename='prediction.log',
+    filename=get_log_filepath('prediction.log'),
     filemode='w',
     level=logging.INFO,
     format='%(asctime)s.%(msecs)03d:%(levelname)s:%(module)s.%(funcName)s: %(message)s',
