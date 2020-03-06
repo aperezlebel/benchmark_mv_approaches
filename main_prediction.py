3fbe7720143a66986f14ebf4562b157a08057503
Alexandre Perez <alexperez@wanadoo.fr>
2020-03-06 11:29:41 -0500

    forgot to close print file

--- main_prediction.py
+++ main_prediction.py
@@ -5,9 +5,12 @@ from prediction.main import main
 logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())  # Print also in console.
 
-sys.stdout = open('prediction_print.log', 'w')
+print_file = open('prediction_print.log', 'w')
+sys.stdout = print_file
 
 logging.basicConfig(filename='prediction.log', filemode='w', level=logging.INFO)#, format='[%(asctime)s] %(message)s')
 logger.info('Started prediction')
 main()
 logger.info('Ended prediction')
+
+print_file.close()
