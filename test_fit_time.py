4d5607fdea653a3c6bd8713ab813409e1e248703
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-03-18 15:53:57 -0400

    Updated test fit time to take argv

--- test_fit_time.py
+++ test_fit_time.py
@@ -12,8 +12,14 @@ logger = logging.getLogger(__name__)
 
 
 def run(argv=None):
-    task = tasks['UKBB/fluid_intelligence_light']
-    # task = tasks['TB/shock_hemo']
+    if argv is None or len(argv) < 2:
+        logger.info('No argv given.')
+        task = tasks['TB/shock_hemo']
+        clf = 'HGBC'
+    else:
+        task = tasks[argv[1]]
+        clf = argv[2]
+        logger.info(f'Argv given. Task {task.meta.tag}. Clf {clf}.')
 
     logger.info('Getting X.')
     X = task.X
@@ -33,11 +39,17 @@ def run(argv=None):
 
     for learning_rate in [0.05, 0.1, 0.3]:
         for max_depth in [3, 6, 9]:
-            estimator = HistGradientBoostingRegressor(
-                loss='least_absolute_deviation',
-                learning_rate=learning_rate,
-                max_depth=max_depth
-            )
+            if clf == 'HGBC':
+                estimator = HistGradientBoostingClassifier(
+                    learning_rate=learning_rate,
+                    max_depth=max_depth
+                )
+            elif clf == 'HGBR':
+                estimator = HistGradientBoostingRegressor(
+                    loss='least_absolute_deviation',
+                    learning_rate=learning_rate,
+                    max_depth=max_depth
+                )
 
             logger.info(f'Params: LR {learning_rate} MD {max_depth}')
             logger.info('Fitting estimator.')
