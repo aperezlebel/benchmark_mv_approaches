bf0662c321e91c285b2dfea1bf1e7d32b82cc201
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-03-18 13:48:27 -0400

    updated test_fit_time

--- test_fit_time.py
+++ test_fit_time.py
@@ -1,7 +1,8 @@
 
 import logging
 from sklearn.experimental import enable_hist_gradient_boosting
-from sklearn.ensemble import HistGradientBoostingRegressor
+from sklearn.ensemble import HistGradientBoostingRegressor, \
+    HistGradientBoostingClassifier
 from sklearn.model_selection import train_test_split
 
 from prediction.tasks import tasks
@@ -12,30 +13,36 @@ logger = logging.getLogger(__name__)
 
 def run(argv=None):
     task = tasks['UKBB/fluid_intelligence_light']
+    # task = tasks['TB/shock_hemo']
 
     logger.info('Getting X.')
     X = task.X
     logger.info('Getting y.')
     y = task.y
 
+    logger.info(f'X shape before splits: {X.shape}')
+
     # Simulate the outer CV (the one of KFold)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    X_train, _, y_train, _ = train_test_split(X, y, test_size=0.2)
 
     # Simulate the inner CV (the one of RandomSearchCV)
-    X_train2, X_test2, y_train2, y_test2 = train_test_split(X_train, y_train, test_size=0.2)
+    X_train2, _, y_train2, _ = train_test_split(X_train, y_train, test_size=0.2)
 
     # Now X has the same shape as in real experiment
     logger.info(f'X shape: {X_train2.shape}')
 
-    estimator = HistGradientBoostingRegressor(
-        loss='least_absolute_deviation',
-        learning_rate=1e-5,
-        max_depth=11
-    )
+    for learning_rate in [0.05, 0.1, 0.3]:
+        for max_depth in [3, 6, 9]:
+            estimator = HistGradientBoostingRegressor(
+                loss='least_absolute_deviation',
+                learning_rate=learning_rate,
+                max_depth=max_depth
+            )
 
-    logger.info('Fitting estimator.')
-    estimator.fit(X_train2, y_train2)
-    logger.info('Estimator fitted.')
+            logger.info(f'Params: LR {learning_rate} MD {max_depth}')
+            logger.info('Fitting estimator.')
+            estimator.fit(X_train2, y_train2)
+            logger.info('Estimator fitted.')
 
 
 
