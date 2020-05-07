20dcdb80f646693df72c5d752ca892cdd064bdfe
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-05-07 12:23:40 +0200

    Add brest cancer prediciton task to UKBB

--- prediction/tasks/UKBB.py
+++ prediction/tasks/UKBB.py
@@ -114,3 +114,62 @@ tasks_meta.append(TaskMeta(
 ))
 
 
+tasks_meta.append(TaskMeta(
+    name='breast',
+    db='UKBB',
+    df_name='40663',
+    predict=None,
+    keep=[
+        '31-0.0',  # Sex
+        '48-0.0',  # Waist circumpherence
+        '2714-0.0',  # Age of menarche
+        '3581-0.0',  # Age of menopause
+        '2724-0.0',  # Had menopause
+        '21001-0.0',  # BMI
+        '23104-0.0',  # BMI
+        '20116-0.0',  # Smoking status
+        '1239-0.0',  # Current tobacco smoking
+        '1249-0.0',  # Past tobacco smoking
+        '22506-0.0',  # Tobacco smoking
+        '201260-0.0',  # Ever smoked
+        '1259-0.0',  # Smoking/smokers in household
+        '3436-0.0',  # Age started smoking in current smokers
+        '2867-0.0',  # Age started smoking in former smokers
+        '20161-0.0',  # Pack years adult smoking as proportion of life span exposed to smoking
+        '20162-0.0',  # Pack years of smoking
+        '22611-0.0',  # Workplace had a lot of cigarette smoke from other people smoking
+        '1787-0.0',  # Maternal smoking around birth
+        '2794-0.0',  # Age started oral contraceptive pill
+        '2794-0.0',  # Age started oral contraceptive pill
+        '2804-0.0',  # Age when last used oral contraceptive pill
+        '2784-0.0',  # Ever taken oral contraceptive pill
+        '132122-0.0',  # Date N80 first reported (endometriosis)
+        '41270-0.0',  # Diagnoses - ICD10
+        '41271-0.0',  # Diagnoses - ICD9
+        '41202-0.0',  # Diagnoses - main ICD10
+        '41203-0.0',  # Diagnoses - main ICD9
+        '41204-0.0',  # Diagnoses - secondary ICD10
+        '41205-0.0',  # Diagnoses - secondary ICD9
+        '41201-0.0',  # External causes - ICD10
+        '40006-0.0',  # Type of cancer: ICD10
+        '40013-0.0',  # Type of cancer: ICD9
+    ],
+    transform=None,
+    classif=True,
+))
+
+
+tasks_meta.append(TaskMeta(
+    name='breast_light',
+    db='UKBB',
+    df_name='40284',
+    predict=None,
+    keep=[
+        '31-0.0',  # Sex
+        '48-0.0',  # Waist circumpherence
+    ],
+    transform=None,
+    classif=True,
+))
+
+
