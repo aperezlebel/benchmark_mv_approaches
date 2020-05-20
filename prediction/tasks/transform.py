a596a7be69b3a6f50b2ae2479cf3b3a04ad8e59c
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-05-20 11:16:20 +0200

    Add get_infos to the transform class

--- prediction/tasks/transform.py
+++ prediction/tasks/transform.py
@@ -11,3 +11,10 @@ class Transform(object):
     transform: Callable[[pd.DataFrame], pd.dataFrame] = lambda x: x
     input_features: List[str]
     output_features: List[str]
+
+    def get_infos(self):
+        """Return a dict containing infos on the object."""
+        return {
+            'input_features': self.input_features,
+            'output_features': self.output_features,
+        }
