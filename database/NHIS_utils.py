c56ee54ef6dabc891e13f6e94d056478da6811e1
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-06-11 12:55:51 +0200

    Update NHIS_utils to create X_income

--- database/NHIS_utils.py
+++ database/NHIS_utils.py
@@ -1,13 +1,25 @@
 """Import NHIS csv data to Postgresql database."""
+from dask import dataframe as dd
 import pandas as pd
 from sqlalchemy import create_engine
 
+from features_type import _dump_feature_types
+from .constants import ORDINAL, CONTINUE_R, CONTINUE_I
 from . import dbs
 
 
+NHIS = dbs['NHIS']
+translation = {
+    'int32': CONTINUE_I,
+    'int64': CONTINUE_I,
+    'float64': CONTINUE_R,
+    'object': ORDINAL,
+}
+
+
 def to_sql():
+    """Import csv files to posgresql database."""
     engine = create_engine('postgresql://alexandreperez@localhost:5432/nhis')
-    NHIS = dbs['NHIS']
 
     for name, path in NHIS.frame_paths.items():
         print(f'\n{name}\n\tReading csv')
@@ -15,6 +27,47 @@ def to_sql():
         print(f'\tLowering columns')
         df.columns = [c.lower() for c in df.columns]
         print(f'\tConverting to sql')
-        df.to_sql(name, engine)
+        df.to_sql(name, engine, if_exists='replace')
 
 
+def create_X_income(tables):
+    """Create the X_income table."""
+    print('\nX_income')
+    household = tables['household']
+    adult = tables['adult']
+    family = tables['family']
+    person = tables['person']
+
+    print('\tMerging...')
+    df = household.merge(
+        family, how='inner', on=['SRVY_YR', 'HHX'],
+        suffixes=('', '%to_drop')
+    ).merge(
+        person, how='inner', on=['SRVY_YR', 'HHX', 'FMX'],
+        suffixes=('', '%to_drop')
+    ).merge(
+        adult, how='left', on=['SRVY_YR', 'HHX', 'FMX', 'FPX'],
+        suffixes=('', '%to_drop')
+    ).dropna(subset=['ERNYR_P']).compute()
+
+    # Remove duplicate columns of the merges
+    print('\tRemoving duplicates...')
+    df = df.loc[:, ~df.columns.str.endswith('%to_drop')]
+
+    # Save new dataframe
+    print('\tSaving...')
+    df.to_csv(f'{NHIS.data_folder}custom/X_income.csv')
+
+    # Compute the feature types and dump them
+    print('\tDumping feature types...')
+    types = {n: translation[str(t)] for n, t in df.dtypes.items()}
+    types = pd.Series(types, index=df.columns)
+    _dump_feature_types(types, NHIS, 'X_income', anonymize=False)
+
+
+def create_table(table_name):
+    """Create the asked table."""
+    tables = {name: dd.read_csv(p) for name, p in NHIS.available_paths.items()}
+
+    if table_name == 'X_income':
+        create_X_income(tables)
