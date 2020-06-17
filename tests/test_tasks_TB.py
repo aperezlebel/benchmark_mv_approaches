fa019b642a210ac99e7f31f644e598e94b3499dc
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-06-17 14:32:13 +0200

    Changed existing task test structure

--- tests/test_tasks_TB.py
+++ tests/test_tasks_TB.py
@@ -1,6 +1,4 @@
 """Test if tasks are correctly loaded."""
-import os
-import yaml
 from unittest import TestCase
 
 from prediction.tasks import tasks
@@ -80,48 +78,9 @@ class test_TB(TestCase):
         y = task.y
 
         n_rows = 12341
-        # n_features = 697
 
-        assert X.shape[0] == n_rows
-        # assert X.shape == (n_rows, n_features)
+        assert X.shape == (n_rows, 99)
         assert y.shape == (n_rows,)
         assert not y.isna().any()
 
         assert task._f_y == meta.predict.output_features
-
-
-# Load some params from custom file
-filepath = 'custom/strategy_params.yml'
-if os.path.exists(filepath):
-    with open(filepath, 'r') as file:
-        params = yaml.safe_load(file)
-else:
-    params = dict()
-
-n_top_pvals = params.get('n_top_pvals', 100)
-
-
-class test_UKBB(TestCase):
-    """Test UKBB tasks."""
-
-    def test_breast(self):
-        """Test breast cancer task."""
-        task = tasks['UKBB/breast_pvals']
-        meta = task.meta
-
-        assert task.is_classif()
-
-        X = task.X
-        y = task.y
-
-        n_rows = 273384
-        n_features = n_top_pvals
-
-        assert X.shape == (n_rows, n_features)
-        assert y.shape == (n_rows,)
-        assert not y.isna().any()
-
-        assert task._f_y == meta.predict.output_features
-        L1 = list(X.columns)
-        L2 = meta.select.output_features
-        self.assertCountEqual(L1, L2)
