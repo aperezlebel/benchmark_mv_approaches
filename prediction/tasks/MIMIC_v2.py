702b0a11ce3403012f3bd3c66027c39cbd20ea1f
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-06-18 11:38:00 +0200

    Create a task accessor to access task with parameters

--- prediction/tasks/MIMIC_v2.py
+++ prediction/tasks/MIMIC_v2.py
@@ -1,5 +1,4 @@
 """Prediction tasks v2 for MIMIC."""
-import yaml
 import os
 import pandas as pd
 from dask import dataframe as dd
@@ -8,18 +7,6 @@ from .task_v2 import TaskMeta
 from .transform import Transform
 from database import dbs
 
-
-task_metas = list()
-
-# Load some params from custom file
-filepath = 'custom/strategy_params.yml'
-if os.path.exists(filepath):
-    with open(filepath, 'r') as file:
-        params = yaml.safe_load(file)
-else:
-    params = dict()
-
-n_top_pvals = params.get('n_top_pvals', 100)
 
 # Define overall tables
 MIMIC = dbs['MIMIC']
@@ -31,139 +18,175 @@ patients_diagnosis = patients.merge(diagnoses_icd.drop(['SEQ_NUM'], axis=1), how
 septic_shock = dd.from_pandas(pd.DataFrame({'ICD9_CODE': ['78552']}), npartitions=1)
 hemo_shock = dd.from_pandas(pd.DataFrame({'ICD9_CODE': ['78559', '99809', '9584']}), npartitions=1)
 
+
 # Task 1: Septic shock prediciton
-# ----------------------------------------------
-# Define y
-def define_predict_septic(df):
-    """Compute y from patients table."""
-    # Ignore given df
-    positives = patients_diagnosis.merge(septic_shock, how='inner', on='ICD9_CODE')#.set_index('SUBJECT_ID')
-    positives = positives.drop_duplicates(subset=['SUBJECT_ID']).set_index('SUBJECT_ID').index
-    positives_idx = positives.compute()
+# -------------------------------
+def septic_task(**kwargs):
+    """Return TaskMeta for septic shock prediction."""
+    # Define y
+    def define_predict_septic(df):
+        """Compute y from patients table."""
+        # Ignore given df
+        positives = patients_diagnosis.merge(septic_shock, how='inner', on='ICD9_CODE')
+        positives = positives.drop_duplicates(subset=['SUBJECT_ID']).set_index('SUBJECT_ID').index
+        positives_idx = positives.compute()
 
-    # Get full idx from df and set the complementary to 0
-    # idx = patients.set_index('SUBJECT_ID').index.compute()
-    idx = df.index
-    # need to intersect because one index of positives_idx is not in idx
-    negatives_idx = idx.difference(positives_idx).intersection(idx)
-    positives_idx = positives_idx.intersection(idx)
+        # Get full idx from df and set the complementary to 0
+        # idx = patients.set_index('SUBJECT_ID').index.compute()
+        idx = df.index
+        # need to intersect because one index of positives_idx is not in idx
+        negatives_idx = idx.difference(positives_idx).intersection(idx)
+        positives_idx = positives_idx.intersection(idx)
 
-    positives = pd.DataFrame({'y': 1}, index=positives_idx)
-    negatives = pd.DataFrame({'y': 0}, index=negatives_idx)
-    df = pd.concat((positives, negatives), axis=0).sort_index()
+        positives = pd.DataFrame({'y': 1}, index=positives_idx)
+        negatives = pd.DataFrame({'y': 0}, index=negatives_idx)
+        df = pd.concat((positives, negatives), axis=0).sort_index()
 
-    return df
+        return df
 
-
-septic_predict_transform = Transform(
-    input_features=[],
-    transform=define_predict_septic,
-    output_features=['y'],
-)
-
-# Define which features to keep
-septic_pvals_dir = 'pvals/MIMIC/septic/'
-septic_idx_path = f'{septic_pvals_dir}used_idx.csv'
-septic_pvals_path = f'{septic_pvals_dir}pvals_filtered.csv'
-if os.path.exists(septic_idx_path) and os.path.exists(septic_pvals_path):
-    pvals = pd.read_csv(septic_pvals_path, header=None,
-                        index_col=0, squeeze=True)
-    pvals = pvals.sort_values()[:n_top_pvals]
-    septic_top_pvals = list(pvals.index)
-
-    septic_pvals_keep_transform = Transform(
-        output_features=septic_top_pvals
-    )
-
-    septic_drop_idx = pd.read_csv(septic_idx_path, index_col=0, squeeze=True)
-
-    septic_idx_transform = Transform(
+    septic_predict_transform = Transform(
         input_features=[],
-        transform=lambda df: df.drop(septic_drop_idx.index, axis=0),
+        transform=define_predict_septic,
+        output_features=['y'],
     )
-else:
-    septic_pvals_keep_transform = None
-    septic_idx_transform = None
 
-task_metas.append(TaskMeta(
-    name='septic_pvals',
-    db='MIMIC',
-    df_name='X_labevents',
-    classif=True,
-    idx_column='subject_id',
-    idx_selection=septic_idx_transform,
-    predict=septic_predict_transform,
-    transform=None,
-    select=septic_pvals_keep_transform,
-    encode_select=None,
-    encode_transform=None,
-))
+    # Define which features to keep
+    if 'RS' in kwargs and 'T' in kwargs:
+        RS = kwargs['RS']
+        T = kwargs['T']
+        septic_pvals_dir = 'pvals/MIMIC/septic_pvals/'
+        septic_idx_path = f'{septic_pvals_dir}RS{RS}-T{T}used_idx.csv'
+        septic_pvals_path = f'{septic_pvals_dir}RS{RS}-T{T}pvals_filtered.csv'
+
+    if (
+        'RS' in kwargs and
+        'T' in kwargs and
+        os.path.exists(septic_idx_path) and
+        os.path.exists(septic_pvals_path)
+    ):
+        pvals = pd.read_csv(septic_pvals_path, header=None,
+                            index_col=0, squeeze=True)
+
+        assert 'n_top_pvals' in kwargs
+        n_top_pvals = kwargs['n_top_pvals']
+        pvals = pvals.sort_values()[:n_top_pvals]
+        septic_top_pvals = list(pvals.index)
+
+        septic_pvals_keep_transform = Transform(
+            output_features=septic_top_pvals
+        )
+
+        septic_drop_idx = pd.read_csv(septic_idx_path, index_col=0,
+                                      squeeze=True)
+
+        septic_idx_transform = Transform(
+            input_features=[],
+            transform=lambda df: df.drop(septic_drop_idx.index, axis=0),
+        )
+
+    else:
+        septic_pvals_keep_transform = None
+        septic_idx_transform = None
+
+    return TaskMeta(
+        name='septic_pvals',
+        db='MIMIC',
+        df_name='X_labevents',
+        classif=True,
+        idx_column='subject_id',
+        idx_selection=septic_idx_transform,
+        predict=septic_predict_transform,
+        transform=None,
+        select=septic_pvals_keep_transform,
+        encode_select=None,
+        encode_transform=None,
+    )
 
 
 # Task 2: Hemorrhagic shock prediciton
 # ------------------------------------
-# Define y
-def define_predict_hemo(df):
-    """Compute y from patients table."""
-    # Ignore given df
-    positives = patients_diagnosis.merge(hemo_shock, how='inner', on='ICD9_CODE')#.set_index('SUBJECT_ID')
-    positives = positives.drop_duplicates(subset=['SUBJECT_ID']).set_index('SUBJECT_ID').index
-    positives_idx = positives.compute()
+def hemo_task(**kwargs):
+    """Return TaskMeta for Hemorrhagic shock prediction."""
+    # Define y
+    def define_predict_hemo(df):
+        """Compute y from patients table."""
+        # Ignore given df
+        positives = patients_diagnosis.merge(hemo_shock, how='inner', on='ICD9_CODE')
+        positives = positives.drop_duplicates(subset=['SUBJECT_ID']).set_index('SUBJECT_ID').index
+        positives_idx = positives.compute()
 
-    # Get full idx from df and set the complementary to 0
-    # idx = patients.set_index('SUBJECT_ID').index.compute()
-    idx = df.index
-    # need to intersect because one index of positives_idx is not in idx
-    negatives_idx = idx.difference(positives_idx).intersection(idx)
-    positives_idx = positives_idx.intersection(idx)
+        # Get full idx from df and set the complementary to 0
+        # idx = patients.set_index('SUBJECT_ID').index.compute()
+        idx = df.index
+        # need to intersect because one index of positives_idx is not in idx
+        negatives_idx = idx.difference(positives_idx).intersection(idx)
+        positives_idx = positives_idx.intersection(idx)
 
-    positives = pd.DataFrame({'y': 1}, index=positives_idx)
-    negatives = pd.DataFrame({'y': 0}, index=negatives_idx)
-    df = pd.concat((positives, negatives), axis=0).sort_index()
+        positives = pd.DataFrame({'y': 1}, index=positives_idx)
+        negatives = pd.DataFrame({'y': 0}, index=negatives_idx)
+        df = pd.concat((positives, negatives), axis=0).sort_index()
 
-    return df
+        return df
 
-
-hemo_predict_transform = Transform(
-    input_features=[],
-    transform=define_predict_hemo,
-    output_features=['y'],
-)
-
-# Define which features to keep
-hemo_pvals_dir = 'pvals/MIMIC/hemo/'
-hemo_idx_path = f'{hemo_pvals_dir}used_idx.csv'
-hemo_pvals_path = f'{hemo_pvals_dir}pvals_filtered.csv'
-if os.path.exists(hemo_idx_path) and os.path.exists(hemo_pvals_path):
-    pvals = pd.read_csv(hemo_pvals_path, header=None,
-                        index_col=0, squeeze=True)
-    pvals = pvals.sort_values()[:n_top_pvals]
-    hemo_top_pvals = list(pvals.index)
-
-    hemo_pvals_keep_transform = Transform(
-        output_features=hemo_top_pvals
-    )
-
-    hemo_drop_idx = pd.read_csv(hemo_idx_path, index_col=0, squeeze=True)
-
-    hemo_idx_transform = Transform(
+    hemo_predict_transform = Transform(
         input_features=[],
-        transform=lambda df: df.drop(hemo_drop_idx.index, axis=0),
+        transform=define_predict_hemo,
+        output_features=['y'],
     )
-else:
-    hemo_pvals_keep_transform = None
-    hemo_idx_transform = None
 
-task_metas.append(TaskMeta(
-    name='hemo_pvals',
-    db='MIMIC',
-    df_name='X_labevents',
-    classif=True,
-    idx_column='subject_id',
-    idx_selection=hemo_idx_transform,
-    predict=hemo_predict_transform,
-    transform=None,
-    select=hemo_pvals_keep_transform,
-    encode_select=None,
-    encode_transform=None,
-))
+    # Define which features to keep
+    if 'RS' in kwargs and 'T' in kwargs:
+        RS = kwargs['RS']
+        T = kwargs['T']
+        hemo_pvals_dir = 'pvals/MIMIC/hemo_pvals/'
+        hemo_idx_path = f'{hemo_pvals_dir}RS{RS}-T{T}used_idx.csv'
+        hemo_pvals_path = f'{hemo_pvals_dir}RS{RS}-T{T}pvals_filtered.csv'
+
+    if (
+        'RS' in kwargs and
+        'T' in kwargs and
+        os.path.exists(hemo_idx_path) and
+        os.path.exists(hemo_pvals_path)
+    ):
+        pvals = pd.read_csv(hemo_pvals_path, header=None,
+                            index_col=0, squeeze=True)
+
+        assert 'n_top_pvals' in kwargs
+        n_top_pvals = kwargs['n_top_pvals']
+        pvals = pvals.sort_values()[:n_top_pvals]
+        hemo_top_pvals = list(pvals.index)
+
+        hemo_pvals_keep_transform = Transform(
+            output_features=hemo_top_pvals
+        )
+
+        hemo_drop_idx = pd.read_csv(hemo_idx_path, index_col=0, squeeze=True)
+
+        hemo_idx_transform = Transform(
+            input_features=[],
+            transform=lambda df: df.drop(hemo_drop_idx.index, axis=0),
+        )
+
+    else:
+        hemo_pvals_keep_transform = None
+        hemo_idx_transform = None
+
+    return TaskMeta(
+        name='hemo_pvals',
+        db='MIMIC',
+        df_name='X_labevents',
+        classif=True,
+        idx_column='subject_id',
+        idx_selection=hemo_idx_transform,
+        predict=hemo_predict_transform,
+        transform=None,
+        select=hemo_pvals_keep_transform,
+        encode_select=None,
+        encode_transform=None,
+    )
+
+
+task_metas = {
+    'septic_pvals': septic_task,
+    'hemo_pvals': hemo_task,
+}
--- prediction/tasks/NHIS_v2.py
+++ prediction/tasks/NHIS_v2.py
@@ -1,5 +1,4 @@
 """Prediction tasks v2 for NHIS."""
-import yaml
 import os
 import pandas as pd
 
@@ -7,124 +6,148 @@ from .task_v2 import TaskMeta
 from .transform import Transform
 
 
-task_metas = list()
-
-# Load some params from custom file
-filepath = 'custom/strategy_params.yml'
-if os.path.exists(filepath):
-    with open(filepath, 'r') as file:
-        params = yaml.safe_load(file)
-else:
-    params = dict()
-
-n_top_pvals = params.get('n_top_pvals', 100)
-
-
-# Task 1: income shock prediciton
-# ----------------------------------------------
-income_predict_transform = Transform(
-    input_features=['ERNYR-P'],
-    output_features=['ERNYR-P'],
-)
-
-# Drop features linked to feature to predict
-income_drop_features = {
-    'INCGRP4',
-    'INCGRP5',
-}
-
-# Define which features to keep
-income_pvals_dir = 'pvals/NHIS/income/'
-income_idx_path = f'{income_pvals_dir}used_idx.csv'
-income_pvals_path = f'{income_pvals_dir}pvals_filtered.csv'
-if os.path.exists(income_idx_path) and os.path.exists(income_pvals_path):
-    pvals = pd.read_csv(income_pvals_path, header=None,
-                        index_col=0, squeeze=True)
-    for f in income_drop_features:  # Drop asked features from pvals
-        pvals = pvals[~pvals.index.str.contains(f)]
-    pvals = pvals.sort_values()[:n_top_pvals]
-    income_top_pvals = list(pvals.index)
-
-    income_pvals_keep_transform = Transform(
-        output_features=income_top_pvals
+# Task 1: income prediciton
+# -------------------------------
+def income_task(**kwargs):
+    """Return TaskMeta for income prediction."""
+    income_predict_transform = Transform(
+        input_features=['ERNYR-P'],
+        output_features=['ERNYR-P'],
     )
 
-    income_drop_idx = pd.read_csv(income_idx_path, index_col=0, squeeze=True)
+    # Drop features linked to feature to predict
+    income_drop_features = {
+        'INCGRP4',
+        'INCGRP5',
+    }
 
-    income_idx_transform = Transform(
-        input_features=[],
-        transform=lambda df: df.drop(income_drop_idx.index, axis=0),
+    # Define which features to keep
+    if 'RS' in kwargs and 'T' in kwargs:
+        RS = kwargs['RS']
+        T = kwargs['T']
+        income_pvals_dir = 'pvals/NHIS/income_pvals/'
+        income_idx_path = f'{income_pvals_dir}RS{RS}-T{T}-used_idx.csv'
+        income_pvals_path = f'{income_pvals_dir}RS{RS}-T{T}-pvals_filtered.csv'
+
+    if (
+        'RS' in kwargs and
+        'T' in kwargs and
+        os.path.exists(income_idx_path) and
+        os.path.exists(income_pvals_path)
+    ):
+        pvals = pd.read_csv(income_pvals_path, header=None,
+                            index_col=0, squeeze=True)
+
+        assert 'n_top_pvals' in kwargs
+        n_top_pvals = kwargs['n_top_pvals']
+        for f in income_drop_features:  # Drop asked features from pvals
+            pvals = pvals[~pvals.index.str.contains(f)]
+        pvals = pvals.sort_values()[:n_top_pvals]
+        income_top_pvals = list(pvals.index)
+
+        income_pvals_keep_transform = Transform(
+            output_features=income_top_pvals
+        )
+
+        income_drop_idx = pd.read_csv(income_idx_path, index_col=0,
+                                      squeeze=True)
+
+        income_idx_transform = Transform(
+            input_features=[],
+            transform=lambda df: df.drop(income_drop_idx.index, axis=0),
+        )
+
+    else:
+        income_pvals_keep_transform = None
+        income_idx_transform = None
+
+    return TaskMeta(
+        name='income_pvals',
+        db='NHIS',
+        df_name='X_income',
+        classif=True,
+        idx_column='IDX',
+        idx_selection=income_idx_transform,
+        predict=income_predict_transform,
+        transform=None,
+        select=income_pvals_keep_transform,
+        encode_select='all',
+        encode_transform=None,
+        drop=income_drop_features,
     )
-else:
-    income_pvals_keep_transform = None
-    income_idx_transform = None
-
-task_metas.append(TaskMeta(
-    name='income_pvals',
-    db='NHIS',
-    df_name='X_income',
-    classif=True,
-    idx_column='IDX',
-    idx_selection=income_idx_transform,
-    predict=income_predict_transform,
-    transform=None,
-    select=income_pvals_keep_transform,
-    encode_select='all',
-    encode_transform=None,
-    drop=income_drop_features,
-))
 
 
 # Task 2: BMI prediciton
-# ----------------------------------------------
-bmi_predict_transform = Transform(
-    input_features=['BMI'],
-    output_features=['BMI'],
-)
+# ----------------------
+def bmi_task(**kwargs):
+    """Return TaskMeta for bmi prediction."""
+    bmi_predict_transform = Transform(
+        input_features=['BMI'],
+        output_features=['BMI'],
+    )
 
-# Drop features linked to feature to predict
-bmi_drop_features = {
-    'AHEIGHT',
-    'AWEIGHTP',
+    # Drop features linked to feature to predict
+    bmi_drop_features = {
+        'AHEIGHT',
+        'AWEIGHTP',
+    }
+
+    # Define which features to keep
+    if 'RS' in kwargs and 'T' in kwargs:
+        RS = kwargs['RS']
+        T = kwargs['T']
+        bmi_pvals_dir = 'pvals/NHIS/bmi/'
+        bmi_idx_path = f'{bmi_pvals_dir}RS{RS}-T{T}-used_idx.csv'
+        bmi_pvals_path = f'{bmi_pvals_dir}RS{RS}-T{T}-pvals_filtered.csv'
+
+    if (
+        'RS' in kwargs and
+        'T' in kwargs and
+        os.path.exists(bmi_idx_path) and
+        os.path.exists(bmi_pvals_path)
+    ):
+        pvals = pd.read_csv(bmi_pvals_path, header=None,
+                            index_col=0, squeeze=True)
+
+        assert 'n_top_pvals' in kwargs
+        n_top_pvals = kwargs['n_top_pvals']
+        for f in bmi_drop_features:  # Drop asked features from pvals
+            pvals = pvals[~pvals.index.str.contains(f)]
+        pvals = pvals.sort_values()[:n_top_pvals]
+        bmi_top_pvals = list(pvals.index)
+
+        bmi_pvals_keep_transform = Transform(
+            output_features=bmi_top_pvals
+        )
+
+        bmi_drop_idx = pd.read_csv(bmi_idx_path, index_col=0, squeeze=True)
+
+        bmi_idx_transform = Transform(
+            input_features=[],
+            transform=lambda df: df.drop(bmi_drop_idx.index, axis=0),
+        )
+
+    else:
+        bmi_pvals_keep_transform = None
+        bmi_idx_transform = None
+
+    return TaskMeta(
+        name='bmi_pvals',
+        db='NHIS',
+        df_name='X_income',
+        classif=False,
+        idx_column='IDX',
+        idx_selection=bmi_idx_transform,
+        predict=bmi_predict_transform,
+        transform=None,
+        select=bmi_pvals_keep_transform,
+        encode_select='all',
+        encode_transform=None,
+        drop=bmi_drop_features,
+    )
+
+
+task_metas = {
+    'income_pvals': income_task,
+    'bmi_pvals': bmi_task,
 }
-
-# Define which features to keep
-bmi_pvals_dir = 'pvals/NHIS/bmi/'
-bmi_idx_path = f'{bmi_pvals_dir}used_idx.csv'
-bmi_pvals_path = f'{bmi_pvals_dir}pvals_filtered.csv'
-if os.path.exists(bmi_idx_path) and os.path.exists(bmi_pvals_path):
-    pvals = pd.read_csv(bmi_pvals_path, header=None,
-                        index_col=0, squeeze=True)
-    for f in bmi_drop_features:  # Drop asked features from pvals
-        pvals = pvals[~pvals.index.str.contains(f)]
-    pvals = pvals.sort_values()[:n_top_pvals]
-    bmi_top_pvals = list(pvals.index)
-
-    bmi_pvals_keep_transform = Transform(
-        output_features=bmi_top_pvals
-    )
-
-    bmi_drop_idx = pd.read_csv(bmi_idx_path, index_col=0, squeeze=True)
-
-    bmi_idx_transform = Transform(
-        input_features=[],
-        transform=lambda df: df.drop(bmi_drop_idx.index, axis=0),
-    )
-else:
-    bmi_pvals_keep_transform = None
-    bmi_idx_transform = None
-
-task_metas.append(TaskMeta(
-    name='bmi_pvals',
-    db='NHIS',
-    df_name='X_income',
-    classif=False,
-    idx_column='IDX',
-    idx_selection=bmi_idx_transform,
-    predict=bmi_predict_transform,
-    transform=None,
-    select=bmi_pvals_keep_transform,
-    encode_select='all',
-    encode_transform=None,
-    drop=bmi_drop_features,
-))
