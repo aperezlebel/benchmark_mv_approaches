ce0423d302164f34203d220499a946379818fe9e
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-06-19 17:18:16 +0200

    Plot Max train size

--- task_description.py
+++ task_description.py
@@ -1,6 +1,8 @@
 """Get some information on all the tasks."""
+import yaml
 import os
 import pandas as pd
+from copy import copy
 import matplotlib
 matplotlib.use('MacOSX')
 import matplotlib.pyplot as plt
@@ -37,7 +39,7 @@ def compute_index_sizes():
     df.to_csv(index_sizes_path)
 
 
-def plot_index_sizes():
+def plot_index_sizes(min_test_size):
     """Plot the stored index sizes. must be computed before."""
     if not os.path.exists(index_sizes_path):
         raise ValueError(f'No index sizes found at {index_sizes_path}')
@@ -49,16 +51,32 @@ def plot_index_sizes():
     df = pd.read_csv(index_sizes_path, index_col=0)
     df = df.sort_values(by='index_size', ascending=False)
     df = df.dropna(subset=['index_size'])
-    sns.set_color_codes('muted')
-    sns.barplot(x='index_size', y='tag', data=df, ax=ax, color='b')
 
+    df['max_train_size'] = df['index_size']*(1-min_test_size)
     print(df)
+
+    sns.set_color_codes('pastel')
+    sns.barplot(x='index_size', y='tag', data=df, ax=ax, color='lightgrey',
+                label=f'Min test size ({100*min_test_size:.1f} %)')
+    patches_to_avoid = copy(ax.patches)
+    sns.set_color_codes('muted')
+    sns.barplot(x='max_train_size', y='tag', data=df, ax=ax, color='b',
+                label='Max train size')
+
+    for p in ax.patches:
+        if p in patches_to_avoid:
+            continue
+        _x = p.get_x() + p.get_width()/2
+        _y = p.get_y() + p.get_height()/2
+        value = int(p.get_width())
+        ax.text(_x, _y, value, ha='right', color='white')
+
+    plt.legend()
     plt.show()
 
 
 
 if __name__ == '__main__':
-    # compute_index_sizes()
-    plot_index_sizes()
+    plot_index_sizes(min_test_size=0.1)
 
 
