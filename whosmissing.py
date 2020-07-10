95a14d96427312ab453672214d55f6b9900a529d
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2020-07-10 11:27:38 +0200

    Add whosmissing to monitor missing predictions

---
+++ whosmissing.py
@@ -0,0 +1,77 @@
+import pandas as pd
+import numpy as np
+
+from prediction.PlotHelperV4 import PlotHelperV4
+
+
+df = pd.read_csv('scores/scores.csv', index_col=0)
+
+
+methods =[
+    'MIA',
+    'Mean',
+    'Mean+mask',
+    'Med',
+    'Med+mask',
+    'Iter',
+    'Iter+mask',
+    'KNN',
+    'KNN+mask',
+]
+
+translate_methods = {
+    'MIA': 0,
+    'Mean': 3,
+    'Mean+mask': 6,
+    'Med': 9,
+    'Med+mask': 12,
+    'Iter': 15,
+    'Iter+mask': 18,
+    'KNN': 27,
+    'KNN+mask': 30,
+}
+
+
+def missing_pt_imputation(df):
+    idx = df.loc[df['tuning_PT'].isna(), 'tuning_PT'].index
+
+    df = df.loc[idx]
+
+    print(df)
+
+    dfgb = df.groupby(by=['db', 'task', 'method'])
+    df = dfgb.agg({'imputation_PT': 'first'})
+    df = df.reset_index()
+
+    print(df)
+
+    for id, subdf in df.groupby(by=['db', 'task']):
+        methods = list(subdf['method'])
+        t_methods = [translate_methods[m] for m in methods]
+        t_methods.sort()
+        db = id[0]
+        task = id[1]
+        print(f'{db}/{task}: {methods}')
+
+
+def missing_scores(df, expected_methods):
+    df = PlotHelperV4.aggregate(df, 'score')
+
+    for id, subdf in df.groupby(by=['size', 'db', 'task']):
+        methods = list(subdf['method'])
+        size = id[0]
+        db = id[1]
+        task = id[2]
+        for m in expected_methods:
+            if m not in methods:
+                print(f'{size}/{db}/{task}: {m} missing')
+            else:
+                n_t = subdf.loc[subdf['method'] == m, 'n_trials']
+                assert len(n_t) == 1
+                n_t = n_t.iloc[0]
+                if ('_pvals' in task and n_t != 5) or ('_pvals' not in task and n_t != 1):
+                    print(f'{size}/{db}/{task}: {m} missing trial: {n_t}/5')
+
+
+if __name__ == '__main__':
+    missing_scores(df, expected_methods=methods)
