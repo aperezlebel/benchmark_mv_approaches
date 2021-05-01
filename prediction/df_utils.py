0bb997e17c578d4730e701734197049eeb77b36a
Alexandre Perez <33580936+alexprz@users.noreply.github.com>
2021-05-01 15:43:42 +0200

    Add get_ranks_tab

--- prediction/df_utils.py
+++ prediction/df_utils.py
@@ -49,3 +49,61 @@ def get_scores_tab(scores_raw, method_order=None, db_order=None, relative=False)
         df = df.round(3)
 
     return df
+
+
+def get_ranks_tab(scores_raw, method_order=None, db_order=None):
+    """Compute article ranks tab from raw scores."""
+
+    df = scores_raw.copy()
+
+    if method_order is not None:
+        df = df[df['method'].isin(method_order)]
+    else:
+        method_order = df['method'].unique()
+
+    df = PlotHelper.aggregate(df, 'score')
+
+    dfgb = df.groupby(['size', 'db', 'task'])
+    df['rank'] = dfgb['score'].rank(method='dense', ascending=False)
+
+    df.set_index(['size', 'db', 'task', 'method'], inplace=True)
+
+    df = pd.pivot_table(df, values='rank', index=['size', 'method'], columns=['db', 'task'])
+
+    if method_order is not None:
+        df = df.reindex(method_order, level=1)
+
+    if db_order is not None:
+        df = df.reindex(db_order, level=0, axis=1)
+
+    avg_on_sizes = df.mean(level=1)
+    avg_on_sizes['size'] = 'AVG'
+    avg_on_sizes = avg_on_sizes.reset_index().set_index(['size', 'method'])
+
+
+    df_with_avg_dbs = pd.concat([df, avg_on_sizes], axis=0)
+
+    avg_on_dbs = df_with_avg_dbs.mean(axis=1, level=0)
+    avg_on_dbs['All'] = avg_on_dbs.mean(axis=1)
+
+    avg_on_dbs.columns = pd.MultiIndex.from_product([['AVG'], avg_on_dbs.columns])
+
+    def to_int(x):  # Convert to int and robust to NaN
+        try:
+            return str(int(x))
+        except:
+            return x
+
+    avg_on_sizes = avg_on_sizes.round(1)
+    avg_on_dbs = avg_on_dbs.round(1)
+
+    df = df.applymap(to_int)
+    df = pd.concat([df, avg_on_sizes], axis=0)
+    df = pd.concat([df, avg_on_dbs], axis=1)
+
+    df.index.rename(['Size', 'Method'], inplace=True)
+    df.columns.rename(['Database', 'Task'], inplace=True)
+
+    print(df)
+
+    return df
