a6ed08ee07d7ecbe729f41fba754cdbe45924b18
Alexandre Pérez <alexperez@wanadoo.fr>
2021-06-17 18:56:07 +0200

    Update correlation table

--- statistics/statistics.py
+++ statistics/statistics.py
@@ -650,8 +650,10 @@ def run_cor(args, graphics_folder, absolute=False, csv=False, prop_only=True):
     df_cor.set_index('n_selected', append=True, inplace=True)
 
     db_order_renamed = [db_rename.get(db, db) for db in db_order]
-
     df_cor = df_cor.reindex(db_order_renamed+['AVG'], level=0, axis=0)
+
+    # Rotate Traumabase
+    df_cor.rename({'Traumabase': '\\rotsmash{Traumabase}'}, axis=0, inplace=True)
 
     # Processing for dumping
     df_cor.index.rename(['Database', 'Task', 'N features'], inplace=True)
