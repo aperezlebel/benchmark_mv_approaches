d26a446be6c309eedc0c498c94f6ac04cbf7e6e9
Alexandre Pérez <alexperez@wanadoo.fr>
2021-10-26 15:20:12 +0200

    Add cpu option

--- launch_MI.py
+++ launch_MI.py
@@ -8,6 +8,7 @@ parser.add_argument('-s', type=int, default=5, dest='chunk_size')
 parser.add_argument('-n', type=int, default=None, dest='train_size')
 parser.add_argument('-p', type=str, default=None, dest='partition')
 parser.add_argument('-t', type=str, default=None, dest='time')
+parser.add_argument('--cpu', type=int, default=40, dest='n_cpus')
 
 args = parser.parse_args()
 
@@ -55,7 +56,7 @@ for method in [20, 24, 22, 26]:
             train_size_option = '' if args.train_size is None else f' --n {args.train_size}'
             partition_option = '' if args.partition is None else f' --partition {args.partition}'
             time_option = '' if args.time is None else f' --time {args.time}'
-            command = f"salloc --ntasks 1 --cpus-per-task 40 --job-name {method}{T}{db[0]}{name}{partition_option}{time_option} srun --pty python main.py predict {task} {method} --RS 0 --T {T} --nbagging 100{train_size_option}"
+            command = f"salloc --ntasks 1 --cpus-per-task {args.n_cpus} --job-name {method}{T}{db[0]}{name}{partition_option}{time_option} srun --pty python main.py predict {task} {method} --RS 0 --T {T} --nbagging 100{train_size_option}"
             session_name = f"{task}_M{method}_T{T}"
             tmux_command = f"tmux new-session -d -s {session_name} '{command}; read'"
 
