dc4a9ec3aa5f1289fae5206f731e0b77bb8e9b50
Alexandre Pérez <alexperez@wanadoo.fr>
2021-10-26 16:34:06 +0200

    Update launch_MI.py

--- launch_MI.py
+++ launch_MI.py
@@ -9,6 +9,8 @@ parser.add_argument('-n', type=int, default=None, dest='train_size')
 parser.add_argument('-p', type=str, default=None, dest='partition')
 parser.add_argument('-t', type=str, default=None, dest='time')
 parser.add_argument('--cpu', type=int, default=40, dest='n_cpus')
+parser.add_argument('-m', type=str, choices=['mi', 'mia'], default='mi', dest='method')
+parser.add_argument('--mem', type=str, default=None, dest='memory')
 
 args = parser.parse_args()
 
@@ -43,11 +45,16 @@ manual_tasks = [
 chunk_size = args.chunk_size
 commands = []
 
-for method in [20, 24, 22, 26]:
+methods = [20, 24, 22, 26] if args.method == 'mi' else [0, 2]
+
+reg_methods = [22, 26] if args.method == 'mi' else [2]
+clf_methods = [20, 24] if args.method == 'mi' else [0]
+
+for method in methods:
     for task in tasks:
-        if task in reg_tasks and method in [20, 24]:
+        if task in reg_tasks and method in clf_methods:
             continue
-        if task not in reg_tasks and method in [22, 26]:
+        if task not in reg_tasks and method in reg_methods:
             continue
 
         db, name = task.split('/')
@@ -56,7 +63,8 @@ for method in [20, 24, 22, 26]:
             train_size_option = '' if args.train_size is None else f' --n {args.train_size}'
             partition_option = '' if args.partition is None else f' --partition {args.partition}'
             time_option = '' if args.time is None else f' --time {args.time}'
-            command = f"salloc --ntasks 1 --cpus-per-task {args.n_cpus} --job-name {method}{T}{db[0]}{name}{partition_option}{time_option} srun --pty python main.py predict {task} {method} --RS 0 --T {T} --nbagging 100{train_size_option}"
+            memory_option = '' if args.memory is None else f' --mem {args.memory}'
+            command = f"salloc --ntasks 1 --cpus-per-task {args.n_cpus} --job-name {method}{T}{db[0]}{name}{partition_option}{time_option}{memory_option} srun --pty python main.py predict {task} {method} --RS 0 --T {T} --nbagging 100{train_size_option}"
             session_name = f"{task}_M{method}_T{T}"
             tmux_command = f"tmux new-session -d -s {session_name} '{command}; read'"
 
