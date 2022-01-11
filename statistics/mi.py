3aa42202bed398f0e0c6bd6ac6c206f7b62131b1
Alexandre Perez-Lebel <alexperez@wanadoo.fr>
2022-01-11 11:23:32 +0100

    Clean comments

--- statistics/mi.py
+++ statistics/mi.py
@@ -146,24 +146,8 @@ def run_multiple_imputation(graphics_folder, n=None, bagging_only=False, linear=
     else:
         method_order = method_order_all
 
-    # reference_method = None if bagging_only else 'MIA'
     reference_method = 'MIA'
 
-    # filepaths = [
-    #     'scores/scores.csv',
-    #     'scores/scores_mi_2500.csv',
-    #     'scores/scores_mi_10000.csv',
-    #     'scores/scores_mi_25000.csv',
-    #     'scores/scores_mi_100000.csv',
-    #     'scores/scores_mia_2500.csv',
-    #     'scores/scores_mia_10000.csv',
-    #     'scores/scores_mia_25000.csv',
-    #     'scores/scores_mia_100000.csv',
-    #     'scores/scores_mean+mask+bagging_2500.csv',
-    #     'scores/scores_mean+mask+bagging_10000.csv',
-    #     'scores/scores_mean+mask+bagging_25000.csv',
-    #     'scores/scores_mean+mask+bagging_100000.csv',
-    # ]
     dfs = [pd.read_csv(path, index_col=0) for path in filepaths]
     scores = pd.concat(dfs, axis=0)
 
